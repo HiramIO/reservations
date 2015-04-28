48147ced79f4203b855bd75c21135f2dc2c77d85
Harrison Taylor <h.taylor@project89.org>
2015-04-28 15:45:10 -0400

    Added reservation math

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,7 @@
 class ReservationsController < ApplicationController
   before_filter :ensure_logged_in, only: [:create, :destroy]
   before_filter :load_restaurant
-  before_filter :load_user
+  # before_filter :load_user
 
   def show
     @reservation = Reservation.find(params[:id])
@@ -13,13 +13,29 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = @restaurant.reservations.build(reservation_params)
-    @reservation.user = current_user
-    if @reservation.save
-      redirect_to restaurants_path, notice: 'reservation created successfully'
+    @reservation.user_id = current_user.id
+    @reservation.restaurant_id = @restaurant.id
+    seats_reserved = @restaurant.reservations.where("time > ? and time < ?", @reservation.time.beginning_of_hour, @reservation.time.beginning_of_hour + 1.hour).map(&:party_size).sum
+
+    # seats_reserved = @restaurant.reservations
+    #                                   .where("date > :start_time AND date < :end_time", {
+    #                                     start_time: @reservation.date,
+    #                                     end_time: @reservation.date + 1.hours
+    #                                   }).map(&:party_size).sum
+
+
+    seats_available = @restaurant.capacity - seats_reserved
+
+    if @reservation.party_size >= seats_available
+      flash[:alert] = "This reservation cannot be made."
+      render :new
     else
-      render 'restaurants/show'
+      @reservation.save
+      flash[:notice] = "Your reservation has been created."
+      redirect_to restaurants_path
     end
   end
+
 
   def destroy
     @reservation = Reservation.find(params[:id])
@@ -28,15 +44,15 @@ class ReservationsController < ApplicationController
 
   private
   def reservation_params
-    params.require(:reservation).permit(:time, :restaurant_id, :user_id )
+    params.require(:reservation).permit(:time, :restaurant_id, :user_id, :party_size)
   end
 
   def load_restaurant
     @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
-  def load_user
-    @user = User.find(params[:user_id])
-  end
+  # def load_user
+  #   @user = User.find(params[:user_id])
+  # end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427201854) do
+ActiveRecord::Schema.define(version: 20150428193249) do
 
   create_table "reservations", force: :cascade do |t|
     t.datetime "time"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150427201854) do
     t.integer  "restaurant_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "party_size"
   end
 
   create_table "restaurants", force: :cascade do |t|
